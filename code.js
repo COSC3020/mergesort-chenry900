c9a9603de2fed422afd4cf7f4823050a7d21dd05
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-03 13:24:54 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
 function mergesort(array) {
+    x = new int[array.size()];
+    for (int i = 0; i < array.size(); i++)
+    {
+        
+    
+    
+    
     return array;
+}
+function split(array, start) {
+    int h = (array.size()/2)-1;
+    half = new int [h];
+    if (start >= h)
+    {   
+         int p = 0;
+        for (int i = start; i < array.size(); i++)
+        {
+            half[p] = array[i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < h; i++)
+        {
+            half[i] = array[i];
+        }
+    }
+    return half;
+    
 }
