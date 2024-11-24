b4e6e94af61e718c057aecbbe055847ba755cba4
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-24 15:09:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function mergesort(array) {
-    x = array.size();
-    for (i = 1; i < array.size();i = i * 2)
+     var x = array.length;
+    for (i = 1; i < array.length;i = i * 2)
         {
             sort(array, i);
         }
@@ -14,10 +14,10 @@ function sort(array, size)
 {
     for (i = 0; i < (array.size() - 1); i + size)//moving through the array by size of split
         {
-            if ((i + size) > array.size())//accounts for odd size values
-                size = i - array.size();
-            two = i + size;
-            one = i;
+            if ((i + size) > array.length)//accounts for odd size values
+                size = i - array.length;
+            var two = i + size;
+            var one = i;
             for (j = one; j < two; j++)
                 {
                     k = two;
